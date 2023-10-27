9d142e12fca78102c546866c87ad0495509a89ba
esteban-almendariz <98506796+esteban-almendariz@users.noreply.github.com>
2023-10-26 23:26:16 -0500

    working on creating reduce hook and dispatch action

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,11 +1,25 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const AuthContext = createContext()
 
+export const authReducer = (state, action) => {
+    switch (action.type) {
+        case 'LOGIN':
+            return {...state, user: action.payload}
+        default: 
+        return state
+    }
+}
+
 export const AuthContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(authReducer, {
+        user: null
+    })
+
+    console.log('AuthContext state', state)
 
     return (
-        <AuthContext.Provider value={{color: 'gray'}}>
+        <AuthContext.Provider value={{...state, dispatch}}>
             {children}
         </AuthContext.Provider>
     )
--- src/hooks/useSignup.jsx
+++ src/hooks/useSignup.jsx
@@ -5,11 +5,13 @@ import { createUserWithEmailAndPassword,
     signOut,
     onAuthStateChanged
    } from "firebase/auth"
+import { useAuthContext } from "./useAuthContext";
 import { auth } from "../config/firebase";
 
 export const useSignup = () => {
     const [error, setError] = useState(null)
     const [isPending, setIsPending] = useState(false)
+    const { dispatch } = useAuthContext()
 
     const signup = async(email, password) => {
         setError(null)
@@ -17,6 +19,8 @@ export const useSignup = () => {
 
         try {
             await createUserWithEmailAndPassword(auth,email, password)
+
+            dispatch({ type: 'LOGIN', payload: auth.currentUser})
 
             setIsPending(false)
             setError(null)
